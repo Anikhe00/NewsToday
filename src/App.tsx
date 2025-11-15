f9b4cc54a95c9ad92f80ebe8f4729312e1c4e2cb
Shakirat Akanji <akanjishakirato@gmail.com>
2025-11-15 23:27:03 +0100

    Scaffold a React Project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-full h-dvh flex items-center justify-center">
+      <p className="text-blue-500">Welcome to Bitume Task</p>
+    </div>
+  );
+}
+
+export default App;
